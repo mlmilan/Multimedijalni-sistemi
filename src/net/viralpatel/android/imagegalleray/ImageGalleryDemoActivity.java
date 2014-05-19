870a65d78aa25484de695d6ba7d1b112a39e98c2
sofija <sofija.puric@gmail.com>
2014-05-19 20:57:03 +0200

    proba

--- src/net/viralpatel/android/imagegalleray/ImageGalleryDemoActivity.java
+++ src/net/viralpatel/android/imagegalleray/ImageGalleryDemoActivity.java
@@ -19,6 +19,8 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -29,19 +31,20 @@ import android.view.ViewGroup.LayoutParams;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class ImageGalleryDemoActivity extends Activity {
     
-	
-	private static int RESULT_LOAD_IMAGE = 1;
 	private String picturePath;
     private Bitmap changeBitmap = null;
     private Bitmap originalBitmap = null;
-    private ImageView imageView;
+    private ImageView imageView, imageViewResized, imageViewResized1, imageViewResized2, imageViewResized3,
+    imageViewResized4, imageViewResized5, imageViewResized6;
     private DrawOnTop mDrawOnTop;
 	private long tStart, tEnd, tElapsed1, tElapsed2, bmpSize1, bmpSize2;
 	private boolean first = true;
@@ -52,28 +55,66 @@ public class ImageGalleryDemoActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.filters);
         
-        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
-        Button filter = (Button) findViewById(R.id.buttonFilter);
-        Button histogram = (Button) findViewById(R.id.buttonHistogram);
-        Button grafik = (Button) findViewById(R.id.buttonGrafik);
-        Button colorPicker = (Button) findViewById(R.id.buttonColorPicker);
-        
+       // Button filter = (Button) findViewById(R.id.buttonFilter);
+       // Button histogram = (Button) findViewById(R.id.buttonHistogram);
+       // Button grafik = (Button) findViewById(R.id.buttonGrafik);
+       // Button colorPicker = (Button) findViewById(R.id.buttonColorPicker);
+        imageView = (ImageView) findViewById(R.id.imgView);
+        imageViewResized = (ImageView) findViewById(R.id.imgViewProba);
+        imageViewResized1 = (ImageView) findViewById(R.id.imgViewProba1);
+        imageViewResized2 = (ImageView) findViewById(R.id.imgViewProba2);
+        imageViewResized3 = (ImageView) findViewById(R.id.imgViewProba3);
+        imageViewResized4 = (ImageView) findViewById(R.id.imgViewProba4);
+        imageViewResized5 = (ImageView) findViewById(R.id.imgViewProba5);
+        imageViewResized6 = (ImageView) findViewById(R.id.imgViewProba6);
         // Restore preferences
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        String putanja = settings.getString("putanja", "");
+        picturePath = settings.getString("putanja", "");
         
         
+		File imageF = new File(picturePath);
+		try {
+			BitmapScaler scaler = new BitmapScaler(imageF, 512);
+			originalBitmap = scaler.getScaled();
+
+                ExifInterface exif = new ExifInterface(picturePath);
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+                Log.d("EXIF", "Exif: " + orientation);
+                Matrix matrix = new Matrix();
+                if (orientation == 6) {
+                    matrix.postRotate(90);
+                }
+                else if (orientation == 3) {
+                    matrix.postRotate(180);
+                }
+                else if (orientation == 8) {
+                    matrix.postRotate(270);
+                }
+                originalBitmap = Bitmap.createBitmap(originalBitmap, 0, 0, originalBitmap.getWidth(), originalBitmap.getHeight(), matrix, true); // rotating bitmap
+           
+            if (originalBitmap.getWidth() <= originalBitmap.getHeight())
+            	imageView.setScaleType(ScaleType.CENTER_CROP);
+            else 
+            	imageView.setScaleType(ScaleType.FIT_CENTER);
+			imageView.setImageBitmap(originalBitmap);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		createSmallerImage();
+		
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             color = extras.getInt("color");
             //Toast.makeText(getBaseContext(), "Boja: " + color, Toast.LENGTH_LONG).show();
             
             imageView = (ImageView) findViewById(R.id.imgView);
-			File imageF = new File(putanja);
+			File imageFile = new File(picturePath);
 			try {
-				BitmapScaler scaler = new BitmapScaler(imageF, 512);
+				BitmapScaler scaler = new BitmapScaler(imageFile, 512);
 				originalBitmap = scaler.getScaled();
 				imageView.setImageBitmap(scaler.getScaled());
 			} catch (IOException e) {
@@ -87,18 +128,19 @@ public class ImageGalleryDemoActivity extends Activity {
 			originalBitmap = changeBitmap;
         }
         
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+        imageViewResized.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				
-				Intent i = new Intent(
-						Intent.ACTION_PICK,
-						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-				
-				startActivityForResult(i, RESULT_LOAD_IMAGE);
+				// TODO Auto-generated method stub
+				changeBitmap = Filter.invert(originalBitmap);
+				originalBitmap.recycle();
+				originalBitmap = null;
+				imageView.setImageBitmap(changeBitmap);
+				originalBitmap = changeBitmap;
 			}
 		});
+        /*
         filter.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -185,7 +227,7 @@ public class ImageGalleryDemoActivity extends Activity {
 		        
 
 		        graphView.addSeries(exampleSeries); // data
-				RelativeLayout layout = (RelativeLayout) findViewById(R.id.rl);
+				FrameLayout layout = (FrameLayout) findViewById(R.id.frame);
 				layout.addView(graphView);
 			}
 		});
@@ -200,45 +242,22 @@ public class ImageGalleryDemoActivity extends Activity {
 				
 			}
 		});
+		*/
     }
+
+	private void createSmallerImage() {
+		// TODO Auto-generated method stub
+
+		Bitmap resized = Bitmap.createScaledBitmap(originalBitmap ,(int)(originalBitmap.getWidth()*0.4), (int)(originalBitmap.getHeight()*0.4), true);
+		Bitmap resizedFilter = Filter.invert(resized);
+		imageViewResized.setImageBitmap(resizedFilter);
+		imageViewResized1.setImageBitmap(resizedFilter);
+		imageViewResized2.setImageBitmap(resizedFilter);
+		imageViewResized3.setImageBitmap(resizedFilter);
+		imageViewResized4.setImageBitmap(resizedFilter);
+		imageViewResized5.setImageBitmap(resizedFilter);
+		imageViewResized6.setImageBitmap(resizedFilter);
+	}
     
-    
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	super.onActivityResult(requestCode, resultCode, data);
-    	
-		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-			Uri selectedImage = data.getData();
-			String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-			Cursor cursor = getContentResolver().query(selectedImage,
-					filePathColumn, null, null, null);
-			cursor.moveToFirst();
-
-			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-			picturePath = cursor.getString(columnIndex);
-			Log.d("PUTANJA", picturePath);
-			cursor.close();
-			
-			imageView = (ImageView) findViewById(R.id.imgView);
-			File imageF = new File(picturePath);
-			try {
-				BitmapScaler scaler = new BitmapScaler(imageF, 512);
-				originalBitmap = scaler.getScaled();
-				imageView.setImageBitmap(scaler.getScaled());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-		    SharedPreferences.Editor editor = settings.edit();
-		    editor.putString("putanja", picturePath);
-
-		      // Commit the edits!
-		    editor.commit();
-		
-		}
-    }
 	    
 }
